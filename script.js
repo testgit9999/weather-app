8aaa231cce2557bdfe8d8266e19c263c5ecd3b22
vladp7ay3r <khalipov.vlad@gmail.com>
2022-08-05 22:41:58 +0300

    weather form with styles

---
+++ script.js
@@ -0,0 +1,7 @@
+  // получаем форму
+  let form = document.forms[0]
+  // получаем элемент
+  let input1 = form.elements[0];
+  let input2 = form.elements[1];
+  console.log(input1)
+  console.log(input2)
