c6d1b2da0e59f02e130e56ec29a273092da9accf
vladp7ay3r <khalipov.vlad@gmail.com>
2022-08-05 23:44:34 +0300

    input validation

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-  // получаем форму
+// проверка на число
+function isNumber(num) {
+	return typeof num === 'number' && !isNaN(num);
+}
+ // получаем форму
   let form = document.forms[0]
-  // получаем элемент
-  let input1 = form.elements[0];
-  let input2 = form.elements[1];
-  console.log(input1)
-  console.log(input2)
+  // получаем коллекцию инпутов
+let inputs = document.querySelectorAll('input')
+inputs.forEach((item)=>{
+	item.addEventListener('blur', (event)=>{
+		if(isNumber(+event.target.value)){
+			event.target.nextElementSibling.hidden = true;
+		}else{
+			event.target.nextElementSibling.hidden = false;	
+		}
+	})
+})
